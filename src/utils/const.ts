863a59b8ef6b1c841855fc3075d5eb3e373f6ed3
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-20 17:27:43 +0900

    feat: apiKey,baseUrlを定数化

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -2,3 +2,6 @@ export const navigations = [
   { name: 'Victory royals', href: '/wins' },
   { name: 'Stats', href: '/stats' },
 ]
+
+export const apiKey = process.env.NEXT_PUBLIC_API_KEY
+export const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
