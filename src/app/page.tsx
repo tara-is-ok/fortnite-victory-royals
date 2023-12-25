d013ff189583ff4c22d77ef58e952f6cf83acca9
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-25 11:40:31 +0900

    feat: add link to top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { ArrowRightCircleIcon } from '@heroicons/react/24/outline'
 import { FC } from 'react'
 
 const Home: FC = () => {
@@ -6,13 +7,20 @@ const Home: FC = () => {
     <div>
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-72">
-          <div className="text-center ">
+          <div className="text-center grid gap-y-5">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               🧊Fortnite Stats🫧
             </h1>
-            <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl mt-5">
+            <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               🪼😶‍🌫️💦💦🧩💦💦🌱💧
             </p>
+            <a
+              href="/wins"
+              className="flex justify-center text-2xl font-semibold leading-6 text-gray-900 underline"
+            >
+              try victory royals
+              <ArrowRightCircleIcon className="w-6 h-6 ml-1" />
+            </a>
           </div>
         </div>
       </div>
