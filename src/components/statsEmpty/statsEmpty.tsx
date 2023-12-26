28f425560f977dcf35850ce87e1ebf14dae0c535
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-26 18:45:47 +0900

    feat: change text size

--- src/components/statsEmpty/statsEmpty.tsx
+++ src/components/statsEmpty/statsEmpty.tsx
@@ -6,7 +6,7 @@ import { Gifs } from '..'
 export const StatsEmpty: FC = () => (
   <>
     <div className="text-gray-700 mt-12 pl-3">
-      <p className="font-bold text-2xl mb-3">{`What is the user's information?`}</p>
+      <p className="font-bold text-xl mb-3">{`What is the user's information?`}</p>
       <div className="flex items-center gap-x-2">
         <p>Please enter the information from </p>
         <UserCircleIcon className="w-7 h-7" />
