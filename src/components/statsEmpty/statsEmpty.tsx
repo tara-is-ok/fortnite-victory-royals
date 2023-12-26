f3c8530d47399495a01c76136cf6dbde1d5490a3
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-26 19:02:21 +0900

    fix: layout

--- src/components/statsEmpty/statsEmpty.tsx
+++ src/components/statsEmpty/statsEmpty.tsx
@@ -5,9 +5,9 @@ import { Gifs } from '..'
 
 export const StatsEmpty: FC = () => (
   <>
-    <div className="text-gray-700 mt-12 pl-3">
+    <div className="text-gray-700 mt-12">
       <p className="font-bold text-xl mb-3">{`What is the user's information?`}</p>
-      <div className="flex items-center gap-x-2">
+      <div className="flex items-center gap-x-1">
         <p>Please enter the information from </p>
         <UserCircleIcon className="w-7 h-7" />
         <p>below !</p>
