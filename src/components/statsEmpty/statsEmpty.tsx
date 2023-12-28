1b118bbd2fa2368a528a936b4aaf872056da8ceb
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-28 12:13:30 +0900

    fix: empty mt

--- src/components/statsEmpty/statsEmpty.tsx
+++ src/components/statsEmpty/statsEmpty.tsx
@@ -5,7 +5,7 @@ import { Gifs } from '..'
 
 export const StatsEmpty: FC = () => (
   <>
-    <div className="text-gray-700 mt-12">
+    <div className="text-gray-700 mt-6">
       <p className="font-bold text-xl mb-3">{`What is the user's information?`}</p>
       <div className="flex items-center gap-x-1">
         <p>Please enter the information from </p>
